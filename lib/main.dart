819069f07ed7fb7d46d1ed710c252eeed6d2179a
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-04-14 11:42:38 +0530

    home screen class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:app_1/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'card.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,90 +21,6 @@ class MyApp extends StatelessWidget {
         textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: size.height * 0.4,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              image: DecorationImage(
-                alignment: Alignment.bottomCenter,
-                fit: BoxFit.fill,
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.5), BlendMode.dstATop),
-                image: AssetImage(
-                  "assets/images/wallpaper.png",
-                ),
-              ),
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                          color: Color(0XFFF06C04), shape: BoxShape.circle),
-                      child: SvgPicture.asset('assets/icons/profile.svg'),
-                    ),
-                  ),
-                  Text("Welcome to Forwards!",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.bold)),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text(
-                      "A knowledge board for the people and writing board on the internet.",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12.0,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 70.0,
-                  ),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      childAspectRatio: 1.12,
-                      crossAxisSpacing: 6.0,
-                      mainAxisSpacing: 6.0,
-                      children: <Widget>[
-                        card(),
-                        card(),
-                        card(),
-                        card(),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
